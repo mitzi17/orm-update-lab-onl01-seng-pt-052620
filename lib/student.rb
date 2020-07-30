3592caf5cf574d8c2d2ff876355fc21cd5f4a387
Mitzi Estrada <m_estrada17@live.com>
2020-07-30 06:50:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,10 +53,11 @@ class Student
   end
   
   def self.new_from_db(row)
-    self.new(id, name, grade)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
+    student = self.new(id, name, grade)
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
   
   
